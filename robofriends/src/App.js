84c8dbc65ab4566f2a8a2b277e8e8a7810ac364c
unknown <marti.molder@gmail.com>
2020-02-11 16:08:22 +0200

    Update

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import './App.css';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+
 
 class App extends Component{
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => {
+				return response.json();
+			})
+			.then(users => {
+				this.setState({ robots: users })
+			});
+		
 	}
 
 	onSearchChange = (event) =>{
@@ -22,7 +34,7 @@ class App extends Component{
 		})
 		return(
 			<div className='tc'>
-				<h1>RoboFriends</h1>
+				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={ this.onSearchChange }/>
 				<CardList robots={ filteredRobots }/>
 			</div>
